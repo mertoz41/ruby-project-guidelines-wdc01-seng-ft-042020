22e3e307a966d3489d4099526ca6e6b02f335d98
Injae Lee <github email address>
2020-05-08 10:08:42 -0400

    deleted_lots_db_communicator

--- lib/db_communicator.rb
+++ lib/db_communicator.rb
@@ -8,68 +8,6 @@ def get_option(num, name)
     end
 end
 
-def add_date_type_price_to_hash(name)
-    hash = Hash.new
-    hash{} 
-
-
-
-def show_driver_rides(name)
-    driver = Driver.find_by(name: name)
-        dd = driver.rides.map do |r|
-            r.date
-            end
-        parse_date = dd.map do |date|
-            DateTime.parse(date).strftime("%m/%d/%Y")
-            end
-        dt = driver.types.map do |t|
-            t.name
-            end
-        dp = driver.rides.map do |p|
-            r.price
-            end
-        hash ={} 
-        driver.rides.each do |ride|
-            hash[Date:] 
-
-            def get_character_films_url(name, hash)
-
-                film_array = []
-                hash["results"].each do |char|
-                  if char["name"] == name
-                    film_array = char["films"]
-                  end
-                end
-
-        {Date: [05/05/2020, 05/05/2020, 05/07/2020],
-         Type: ["Taxi", "Taxi", "Tour"], 
-         Price: [40, 50, 100]}
-
-
-        [ {Date: 05/05/2020, Type: "Event", Price: 90},
-        {Date: 05/05/2020, Type: "Event", Price: 90}
-        {Date: 05/07/2020, Type: "Event", Price: 90} ]
-
-
-    driver.rides.each do |r|
-        hash[date:] r.date
-    end
-    
-     Date: , Type: , Price:            
-
-    puts "#{driver.name}, #{dt}, #{driver.rides.first.type.name}, $#{driver.rides.first.price}"
-        
-end
-
-# def get_log_from_driver
-#     input = gets.chomp
-#     if input.strip.empty?
-#         puts "Please log everything"
-#         input = get_log_from_driver
-#     end 
-#     return input
-# end
-
 def get_driver_log(name)
     puts "__Let's log!"
     puts "__What is the date? (type ex: 06/03/2020)"
